9e87abaf68cef705a9708c6a3d654f847675fb75
GCUCoding <maxdweber7@gmail.com>
2021-11-11 19:01:15 -0700

    Updated Some Files

--- src/main/java/com/gcu/GvisionSiteApplication.java
+++ src/main/java/com/gcu/GvisionSiteApplication.java
@@ -2,10 +2,11 @@ package com.gcu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.gcu")
 public class GvisionSiteApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GvisionSiteApplication.class, args);
 	}
