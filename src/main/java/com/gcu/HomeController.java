2ad2eaf6637ef0466b52006fa0dbbd62ed7ea65f
Vrijesh Patel <vrijeshpatel1997@gmail.com>
2021-11-18 16:21:57 -0700

    information submission button

--- src/main/java/com/gcu/HomeController.java
+++ src/main/java/com/gcu/HomeController.java
@@ -1,13 +1,18 @@
 package com.gcu;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 import org.springframework.web.servlet.ModelAndView;
 
 
 
-@RestController
+@Controller
 public class HomeController {
 
 	
@@ -15,13 +20,33 @@ public class HomeController {
 
 
 
-@RequestMapping("/payment")
+@GetMapping("/payment")
 
-public ModelAndView getPayment() {
+public String getPayment(Model model) {
 	
+	model.addAttribute("processinformation", new FormSubmission());
+	return "payment";
+	
+	
+}
+
+@RequestMapping("/processpayment")
+public ModelAndView processPayment() {
+	
+
 	ModelAndView modelAndView = new ModelAndView();
-	modelAndView.setViewName("payment");
+	modelAndView.setViewName("paymentsubmission");
 	return modelAndView;
+	
+}
+
+
+
+@PostMapping("/informationsubmit")
+public String informationSubmit(@ModelAttribute FormSubmission formsubmission ,Model model) {
+	
+	model.addAttribute("processinformation", formsubmission);
+	return "paymentsubmission";
 }
 
 @RequestMapping("/contactus")
@@ -37,7 +62,7 @@ public ModelAndView getContact() {
 
 @RequestMapping("/")
 
-public ModelAndView gehome() {
+public ModelAndView gethome() {
 	
 	ModelAndView modelAndView = new ModelAndView();
 	modelAndView.setViewName("home");
--- src/main/java/com/gcu/controllers/ComingSoonController.java
+++ src/main/java/com/gcu/controllers/ComingSoonController.java
@@ -1,12 +1,13 @@
 package com.gcu.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+
+
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
-@RequestMapping("/comingSoon")
+@RestController
+
 /**
  * A page used to show the artists upcoming projects
  * @author Max Weber
